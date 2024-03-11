ea2f3064e781d5e7f64df670ef8d3ba6fc2a41c9
Kauê Tribess <kaueirontribess@gmail.com>
2024-03-11 18:18:00 -0300

    Created the top priority apps

--- project/urls.py
+++ project/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('app_main.urls')), # Home
+    path('users/', include('app_users.urls')), # User managemant
+    path('legends/', include('app_legends.urls')), # Legends system
+    # path('d&d/'), # D&D system
 ]
